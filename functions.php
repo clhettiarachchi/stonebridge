d3ef434ffa808b217773b8732406675a79fa821f
Chandika Hettiarachchi <chandikalakshan@gmail.com>
2026-05-03 09:48:16 +0530

    style: project grid, card layout, and status badge colours

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@
  */
 
 require_once get_template_directory() . '/inc/cpt-projects.php';
+
+function sb_enqueue_assets()
+{
+    if (is_post_type_archive('sb_project')) {
+        wp_enqueue_style(
+            'sb-projects',
+            get_template_directory_uri() . '/assets/css/projects.css',
+            [],
+            '1.0.0'
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'sb_enqueue_assets');
